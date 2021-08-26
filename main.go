590efb613048bb696f92f4eed407a4b2780585b8
Andreas Lind <Andreaslind31@gmail.com>
2021-08-26 14:24:53 +0200

    authentication added

--- main.go
+++ main.go
@@ -20,8 +20,12 @@ func main() {
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexGetHandler).Methods("GET")
-	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	r.HandleFunc("/", authRequired(indexGetHandler)).Methods("GET")
+	r.HandleFunc("/", authRequired(indexPostHandler)).Methods("POST")
+	r.HandleFunc("/confirm", authRequired(confirmGetHandler)).Methods("GET")
+	r.HandleFunc("/confirm", authRequired(confirmPostHandler)).Methods("POST")
+	r.HandleFunc("/create", adminRequired(createGetHandler)).Methods("GET")
+	r.HandleFunc("/create", adminRequired(createPostHandler)).Methods("POST")
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
 	r.HandleFunc("/login", loginPostHandler).Methods("POST")
 	r.HandleFunc("/register", registerGetHandler).Methods("GET")
@@ -31,13 +35,35 @@ func main() {
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
-func indexGetHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	_, ok := session.Values["username"]
-	if !ok {
-		http.Redirect(w, r, "/login", http.StatusFound)
-		return
+func adminRequired(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session")
+
+		// Check if user is authenticated - if not send to login page
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+
+		handler.ServeHTTP(w, r)
 	}
+}
+
+func authRequired(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session")
+
+		// // Check if user has username - if not send to login page
+		_, ok := session.Values["username"]
+		if !ok {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+
+		handler.ServeHTTP(w, r)
+	}
+}
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 	comments, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -80,9 +106,23 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	session, _ := store.Get(r, "session")
 	session.Values["username"] = username
+
+	if username == "admin" {
+		session.Values["authenticated"] = true
+	}
+
 	session.Save(r, w)
 	http.Redirect(w, r, "/", http.StatusFound)
 }
+
+// func logoutHandler(w http.ResponseWriter, r *http.Request) {
+// 	session, _ := store.Get(r, "username")
+
+// 	// Revoke users authentication
+// 	session.Values["username"] = nil
+// 	session.Values["authenticated"] = false
+// 	session.Save(r, w)
+// }
 
 func registerGetHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "register.html", nil)
@@ -105,4 +145,16 @@ func registerPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Redirect(w, r, "/login", http.StatusFound)
+}
+func confirmGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "confirm.html", nil)
+}
+func confirmPostHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "confirm.html", nil)
+}
+func createGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "create.html", nil)
+}
+func createPostHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "create.html", nil)
 }
